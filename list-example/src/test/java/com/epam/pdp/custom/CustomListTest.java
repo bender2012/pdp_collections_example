1b48d2fb5285cb1f6739f24b534e629d9727ce28
bender20112 <netant@inbox.ru>
2013-03-18 17:11:25 +0300

    Rewritten test.

--- list-example/src/test/java/com/epam/pdp/custom/CustomListTest.java
+++ list-example/src/test/java/com/epam/pdp/custom/CustomListTest.java
@@ -1,27 +1,30 @@
 package com.epam.pdp.custom;
 
 import static org.junit.Assert.*;
-
 import java.util.List;
-
 import org.junit.Test;
 
 public class CustomListTest {
-
+		
 	private static final String TEST_STRING = "TEST";
+	private static final String SECOND_TEST_STRING = "TEST_2";
 	private static final int POSITION_TO_INSERT = 5;
+	private static final String LINE = "--------------------"; 
 
 	@Test
 	public void testSize() {
+		System.out.println("testSize():");		
 		List<String> customList = new CustomList<String>();
 		assertEquals(POSITION_TO_INSERT, customList.size());
+		System.out.print("Empty list: ");
+		System.out.println(customList.toString());		
 		customList.add(TEST_STRING);
 		assertEquals(POSITION_TO_INSERT + 1, customList.size());
-	}
-
-	@Test
-	public void testRemoveRange() {
-		// fail("Not yet implemented");
+		customList.add(SECOND_TEST_STRING);
+		assertEquals(POSITION_TO_INSERT + 2, customList.size());		
+		System.out.print("List with added elements: ");
+		System.out.println(customList.toString());
+		System.out.println(LINE);
 	}
 
 	@Test
@@ -44,22 +47,16 @@ public class CustomListTest {
 
 	@Test
 	public void testAddT() {
-		// fail("Not yet implemented");
+		List<String> customList = new CustomList<String>();
+		assertEquals(POSITION_TO_INSERT, customList.size());
+		customList.add(TEST_STRING);
+		assertEquals(POSITION_TO_INSERT + 1, customList.size());
+		assertEquals(TEST_STRING, customList.get(POSITION_TO_INSERT));
+		customList.add(SECOND_TEST_STRING);
+		assertEquals(POSITION_TO_INSERT + 2, customList.size());
+		assertEquals(SECOND_TEST_STRING, customList.get(POSITION_TO_INSERT));		
 	}
-
-	@Test
-	public void testRemoveInt() {
-		// fail("Not yet implemented");
-	}
-
-	@Test
-	public void testRemoveObject() {
-		// fail("Not yet implemented");
-	}
-
-	@Test
-	public void testRemoveAllCollectionOfQ() {
-		// fail("Not yet implemented");
-	}
+	
+	//TODO: Test remove element
 
 }
